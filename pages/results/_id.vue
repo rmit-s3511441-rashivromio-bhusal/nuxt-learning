b5894eebe1b8697229db11d34c3ca34f5933fffa
Rashiv Romio <rashivromio@gmail.com>
2018-06-20 11:58:25 +1000

    created card inside components and fetch data from ITUNES

--- pages/results/_id.vue
+++ pages/results/_id.vue
@@ -1,11 +1,8 @@
 <template>
   <div>
     <h1>Results for {{$route.params.id}}</h1>
-
-{{albumData}}
-
     <div v-if="albumExists">
-      <!-- <div v-for="(album, index) in albumData">
+       <div v-for="(album, index) in albumData">
         <Card
           :title="album.collectionCensoredName"
           :image="album.artworkUrl100"
@@ -14,7 +11,7 @@
           :color="picker(index)"
         />
 
-      </div> -->
+      </div> 
 
     </div>
     <div v-else>
@@ -25,7 +22,7 @@
 </template>
 <script>
 import axios from 'axios';
-//import Card from '~/components/Card.vue';
+import Card from '~/components/Card.vue';
 export default {
   asyncData({params}) {
     return axios.get(`https://itunes.apple.com/search?term=${params.id}&entity=album`)
@@ -34,12 +31,12 @@ export default {
       });
   },
   components: {
-  //    Card
+      Card
   },
   middleware: 'search',
   methods: {
     picker(index) {
-      return index % 2 == 0 ? 'red' : 'blue';
+      return index % 2 == 0 ? 'green' : 'pink';
     }
   },
   computed: {
